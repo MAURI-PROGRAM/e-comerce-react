c7397f38d772632f698593b19ba3865ec8ce166d
unknown <merchan.ber.mau@gmail.com>
2019-04-02 19:55:23 -0500

    Creacion de componente proveedor y renderizado de la lista de productos

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+import {ProductConsumer} from '../context';
+
 
 export default class Product extends Component {
+  
   render() {
+    const {id,title,img,price,inCart}=this.props.product;
     return (
-      <div>
-        <h3>Hello from product</h3>
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+      <div className="card">
+        <div className="img-container p-5" onCLick={console.log('tu diste click en esta img')}>
+          <Link to="/details">
+            <img src={img} alt="product" className="card-img-top"></img>
+          </Link>
+          <button className="cart-btm" disabled={inCart?true:false} onClick={()=>{console.log('añadido al carro')}}>
+          {inCart?(<p className="text-capitalize mb-0" disabled>in cart</p>):(<i className="fas fa-cart-plus"></i>)}
+          </button>
+        </div>
       </div>
+      </ProductWrapper>
     )
   }
 }
+
+const ProductWrapper = styled.div`
+`
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
-import Product from './Product'
+import Title from './Title'
+import {storeProducts} from '../data'
+import {ProductConsumer} from '../context'
+import Product from './Product';
 export default class ProductList extends Component {
+  state = {
+    products:storeProducts
+  }
+
   render() {
+   
     return (
       <div>
-        <Product/>
+        <React.Fragment>
+          <div className="py-5">
+            <div className="container">
+              <Title name="our" title="product"/>
+              <div className="row">
+                <ProductConsumer>
+                  {(value)=>{
+                    return value.products.map(products=>{
+                      return <Product key={products.id} product={products}></Product>
+                    })
+                  }}
+                </ProductConsumer>              
+              </div>
+            </div>
+          </div>
+        </React.Fragment>
       </div>
     )
   }
