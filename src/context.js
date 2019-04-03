8346f19a4c15e489b8c805c840c7f6b052014b63
unknown <merchan.ber.mau@gmail.com>
2019-04-03 18:11:56 -0500

    almacenar dinamicamente

--- src/context.js
+++ src/context.js
@@ -6,8 +6,22 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products:storeProducts,
+    products:[],
     detailProduct:detailProduct
+  }
+  componentDidMount(){
+    this.setProducts();
+  }
+  setProducts=()=>{
+    let temProducts=[];
+    storeProducts.forEach(item=>{
+      const singleItem={...item};
+      temProducts=[...temProducts,singleItem];
+
+    });
+    this.setState(()=>{
+      return {products:temProducts};
+    })
   }
   handelDetail=()=>{
     console.log('hello from detail')
@@ -15,12 +29,28 @@ class ProductProvider extends Component {
   addToCart=()=>{
     console.log('hello from add to cart')
   }
+  tester=()=>{
+    console.log('state products:',this.state.products[0].inCart)
+    console.log('Data products : ',storeProducts[0].inCart)
+
+    const tempProducts = [...this.state.products];
+    tempProducts[0].inCart=true
+    this.setState(()=>{
+      return {products:tempProducts}
+    },()=>{
+      console.log('state products:',this.state.products[0].inCart)
+    console.log('Data products : ',storeProducts[0].inCart)
+    })
+  }
   render() {
     return (
       <ProductContext.Provider value={{...this.state,handelDetail:this.handelDetail,
-      addToCart:this.addToCart}}>{
+      addToCart:this.addToCart}}>
+      <button onClick={this.tester}>test me</button> 
+      {
           this.props.children
-      }        
+      }
+             
       </ProductContext.Provider>
     )
   }
